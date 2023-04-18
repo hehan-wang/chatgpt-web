4235e239d0d554a3c1d88b3e0d6c47bc79312850
wangmouwen <454375214@qq.com>
2023-04-18 20:09:06 +0800

    feat: a

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -1,12 +1,23 @@
 import { isNotEmptyString } from '../utils/is'
 
+const ipAccessCache = {}
+
 const auth = async (req, res, next) => {
   const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
   if (isNotEmptyString(AUTH_SECRET_KEY)) {
     try {
       const Authorization = req.header('Authorization')
-      if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim())
-        throw new Error('Error: 无访问权限 | No access rights')
+      if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim()) {
+        if (ipAccessCache[clientIp] === undefined)
+          ipAccessCache[clientIp] = 1
+        else
+          ipAccessCache[clientIp]++
+
+        if (ipAccessCache[clientIp] > 5)
+          throw new Error('Error: 无访问权限 | No access rights')
+      }
       next()
     }
     catch (error) {
