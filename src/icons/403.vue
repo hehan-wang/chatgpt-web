ed981a7138430458f8e3bf764aebfb410f4f0487
wanghehan <454375214@qq.com>
2023-06-14 11:10:28 +0800

    fix(403): add 403

--- src/icons/403.vue
+++ src/icons/403.vue
@@ -4,6 +4,8 @@
   <!--  </div> -->
   <div class="w-[300px] m-auto" style="text-align:center;">
     <p style="margin-top: 10px; line-height: 1.8; font-size: 13px;">
+      <br><span style="color: #e9415a; font-weight: bold;"> 推荐使用更稳定的新网站
+        <br> <a href="https://chat.wenwen-ai.com">https://chat.wenwen-ai.com</a></span><br>
       扫码或搜索公众号：<span style="color: #E9405A; font-weight: bold;">程序员鹤涵</span>
       回复：<span style="color: #e9415a; font-weight: bold; font-size: 17px; margin-bottom: 45px;">403</span>
       <br>即可获得<span style="color: #e9415a; font-weight: bold;"> 验证码 </span>解锁 🔓
